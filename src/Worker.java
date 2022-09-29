36d3f40625d2378abb5f92860fa4173ec5383076
Viner Gindullin <Taratheliberator@gmail.com>
2022-09-29 14:02:43 +0500

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,35 @@
+public class Worker {
+
+    private OnTaskDoneListener callback;
+
+    private OnTaskErrorListener errorCallback;
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+            if (i != 33) {
+                callback.onDone("Task " + i + " is done");
+            } else {
+                errorCallback.onError("Task " + i + " is not done");
+            }
+        }
+
+    }
+
+}
